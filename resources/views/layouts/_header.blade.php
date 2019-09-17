845166cbaf61ccf3aa57d1548aa8a819c9c8642a
䓍上飞 <280267662@qq.com>
2019-09-17 21:41:34 +0800

    登录成功后导航逻辑

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,9 +2,38 @@
     <div class="container">
         <a class="navbar-brand" href="{{route('home')}}">Weibo App</a>
         <ul class="navbar-nav justify-content-end">
-            <li class="nav-item"><a class="nav-link" href="{{route('help')}}"> 帮助</a></li>
-            <li class="nav-item" ><a class="nav-link" href="#">登录            </a></li>
+            @if(Auth::check())
+
+                <li class="nav-item"><a class="nav-link" href="#">用户列表</a></li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" id="navbarDropdown" role="button">
+                    {{Auth::user()->name}} <span class="caret"></span>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{route('users.show',Auth::user())}}">个人中心</a>
+                    <a href="#" class="dropdown-item">编辑资料</a>
+                    <div class="dropdown-divider"></div>
+                    <a href="#" id="logout" class="dropdown-item">
+                        <form action="{{route('logout')}}" method="post">
+                            @csrf
+                            {{method_field('DELETE')}}
+                             <button class="btn btn-primary btn-danger" type="submit" name="submit">
+                               退出
+                             </button>
+                        </form>
+                    </a>
+                </div>
+            </li>
             <li class="nav-item" ><a class="nav-link" href="{{route('signup')}}">注册            </a></li>
+
+                @else
+
+                <li class="nav-item"><a class="nav-link" href="{{route('help')}}"> 帮助</a></li>
+                <li class="nav-item" ><a class="nav-link" href="#">登录            </a></li>
+             @endif
+
         </ul>
+
+
     </div>
 </nav>
